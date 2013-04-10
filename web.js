3f661795908d2494c518c72d55beb292164ecc19
r2fresh <r2fresh@r2fresh-Aspire-1410.(none)>
2013-04-10 16:49:29 +0900

    github add

--- web.js
+++ web.js
@@ -7,6 +7,8 @@ app.get('/',function(request, response){
 });
 
 var port = process.env.PORT || 5000;
+var node_env = process.env.NODE_ENV;
 app.listen(port, function(){
+	console.log("Node_env " + node_env);
 	console.log("Listen on " + port);
 })
